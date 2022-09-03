049efd73e275b9c92f3ae52360d7f04fea08f44b
Abubakar Lawwal <40341812+Abumib@users.noreply.github.com>
2022-09-03 18:31:28 +0100

    Corrected typos in jobs info

--- src/jobsInfo.js
+++ src/jobsInfo.js
@@ -2,16 +2,16 @@ const jobInfo = [
     { 
         role: "National Youth Service @ NITDA",
         period: "March 2022 - Present",
-        workDesc1: "studying algorithms and data structure to implement efficient software",
-        workDesc2: "Learning about API calls and how to connect differnet parts of an application to function as one.",
-        workDesc3: "Modeling using UML class, sequence and UseCase diagrams."
+        workDesc1: "Studying algorithms and data structure to implement efficient algorithms",
+        workDesc2: "Learning about API calls and how to connect different parts of an application to function as one.",
+        workDesc3: "Modeling using UML class, sequence, and UseCase diagrams."
     },
     { 
         role: "Dorm Secretary @ AUN",
         period: "January 2019 - January 2020",
         workDesc1: "Organized meetings with dorm executive",
-        workDesc2: "Helped planned events for the dorm",
-        workDesc3: "Strucutred issues, complaints, suggestions by students to the comity and much more."
+        workDesc2: "Helped plan events for the dorm",
+        workDesc3: "Structured issues, complaints, suggestions by students to the committee, and much more."
     }
 ]
 
